73bde0aad7e673f3f96a841b51e48b2d1f214c50
Guy <nevandnoiz@gmail.com>
2019-03-03 14:36:11 +0200

    Home page

--- js/pages/appsus-home-cmp.js
+++ js/pages/appsus-home-cmp.js
@@ -1,27 +1,44 @@
 import navBar from '../cmps/nav-bar.js';
+import homeService from '../services/home-service.js';
 
 export default {
     template: `   
     <div class="home-page" ref=home>
         
         <nav-bar></nav-bar>
-
-        <button class="change-background" @click="changeBcg">change background</button>
+    <div class="change-background">
+        <button class="change-background-btn" @click="switchBackground('left')"><i class="fas fa-arrow-left"></i></button>
+        <button class="change-background-btn" @click="switchBackground('right')"><i class="fas fa-arrow-right"></i></button>
+    </div>
+        
     </div>
 `,
     data() {
         return {
-            nextImg: 2
+            currBackground: null
         }
     },
     methods: {
-        changeBcg(){
-            const home =this.$refs.home
-            home.style.backgroundImage = `url('/img/${this.nextImg}.jpg')`
-            if (this.nextImg === 9)  this.nextImg = 0
-            this.nextImg++
+        switchBackground(direction) {
+            if (direction === 'right') {
+                this.currBackground++
+                if (this.currBackground === 9) this.currBackground = 1
+            }
+            else if (direction === 'left') {
+                this.currBackground--
+                if (this.currBackground === 0) this.currBackground = 9
+            }
+            this.setBackground()
+            this.saveCurrBackground();
+        },
+        setBackground() {
+            console.log(this.currBackground)
+            const home = this.$refs.home
+            home.style.backgroundImage = `url('/img/${this.currBackground}.jpg')`
+        },
+        saveCurrBackground() {
+            homeService.setBackground(this.currBackground)
         }
-    
     },
     components: {
         navBar
@@ -30,6 +47,8 @@ export default {
 
     },
     created() {
-
-    }
+        homeService.getBackground()
+            .then(background => this.currBackground = background)
+            .then(() => this.setBackground())
+    },
 }
---
+++ js/services/home-service.js
@@ -0,0 +1,27 @@
+import { getFromStorage, saveToStorage} from './util-service.js'
+
+export default {
+    getBackground,
+    setBackground
+}
+
+var gCurrBackground = loadBackground();
+
+function loadBackground() {
+        if (getFromStorage('home-bcg')) return getFromStorage('home-bcg');
+        else {
+            var background = 1;
+            saveToStorage('home-bcg', background)
+            return background;
+        }
+    }
+
+function getBackground() {
+    return Promise.resolve(gCurrBackground);
+}
+
+function setBackground(background) {
+    console.log(typeof background)
+    gCurrBackground = background;
+    saveToStorage('home-bcg', gCurrBackground)
+}
